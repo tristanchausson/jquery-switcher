53819adbdbb7092fb949350c5e5f0b329ac65d39
ldevernay <boblehamster@yahoo.fr>
2017-01-02 15:02:52 +0100

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+var species = {
+	cat    : "cat.jpg",
+	bear   : "bear.jpg",
+	fish   : "fish.jpg"
+};
+
+function main(){
+	/* 
+	tout votre code doit se trouver dans cette fonction,
+	vous pouvez biensur créer d'autres fonctions si nécessaire
+	*/
+
+	// 1. placez un listener sur le click des éléments <button>
+	// 2. dans le callback récupérez le data attribute animal
+	// 3. récupérerez dans l'objet species la valeur correspond à l'attribut récupéré
+	// 4. ajoutez élément img en assignant à son attibut src la valeur précédemment récupérée
+	// 5. inserez cet élément dans l'élément ayant l'id holder
+	// 
+	// Conseils : avancez étape par étape, console.log et debugger autant que nécessaires !
+	
+	
+	 
+}
+
+$(document).ready(function(){
+	main();
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+Ouvrez dans le fichier html dans votre navigateur et arrangez-vous pour que ça marche!
